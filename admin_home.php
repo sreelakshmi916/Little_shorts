8d0278c8b39bc67b0ab355c9013f971322ca4395
Sreelakshmi Venoj <venojsreelakshmi76@gmail.com>
2026-09-07 22:25:14 +0530

    Update admin_home.php

--- admin_home.php
+++ admin_home.php
@@ -1,17 +1,27 @@
+<?php
+include "../db.php";
+?>
 <!DOCTYPE html>
 <html>
 <head>
 <title>LITTLE_SHORTS</title>
 </head>
+
 <body>
-	<h1>Little Shorts Admin Panel</h1><br>
-	Little Shots, Big Protection - Never Miss Your Baby's Vaccine.<br>
-	<p>Welcome Admin!!</p>
-	<nav>
-	<a href="manage_babies.php">Manage Babies</a>
-	<a href="manage_vaccines.php">Manage Vaccines</a>
-	<a href="manage_vaccination_records.php">Manage Records</a>
-	<a href="../index.php">Logout</a>
-	</nav>
+
+<h1>Little Shots Admin Panel</h1>
+
+<p>Welcome Admin!!</p>
+
+<nav>
+
+<a href="admin_dashboard.php">Dashboard</a><br><br>
+
+<a href="manage_vaccines.php">Manage Vaccines</a><br><br>
+
+<a href="../index.php">Logout</a>
+
+</nav>
+
 </body>
 </html>
